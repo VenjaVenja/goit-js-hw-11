156623f01d6f1a63c3e1835f9f7eecb9c3e4f21b
Denis Venher <vendenven@gmail.com>
2022-01-18 09:57:23 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 import './sass/main.scss';
-import ImagesApiFetch from './js/fetch-api-server'
-// import './style.css'
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import ImagesApiFetch from './js/fetch-api-server'
 import galleryTpl from './templates/gallaryMarkup.hbs';
-import scroll from './js/smooth-scroll';
 import LoadMoreBtn from './js/load-more-btn';
+import scroll from './js/smooth-scroll';
 
 
 const refs = {
     searchForm: document.querySelector('#search-form'),
     galleryWrap: document.querySelector('.gallery'),
     messageEl: document.querySelector('.end-collection-message')
-    // loadMoreBtn: document.querySelector('.load-more')
 };
 
 refs.messageEl.classList.add('is-hidden');
@@ -30,7 +28,7 @@ loadMoreBtn.refs.button.addEventListener('click', onLoadMore);
 
 // ====================================onSearch========================================
 
-function onSearch(event) {
+async function onSearch(event) {
     event.preventDefault();
 
     cleargalleryMarkup();
@@ -44,39 +42,48 @@ function onSearch(event) {
 
     loadMoreBtn.show();
     loadMoreBtn.disable();
-    // loadMoreBtn.enable();
     imagesApiFetch.resetPage();
-    imagesApiFetch.fetchPixabayImages().then(data => {
-            
-            if (data.totalHits === 0) {
-                Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-            }
-            if (data.totalHits > 0) {
-                Notify.success(`Hooray! We found ${data.totalHits} images.`);
 
-                appendgalleryMarkup(data.hits);
-                loadMoreBtn.enable();
-        };
-        });
-    
+    const data = await imagesApiFetch.fetchPixabayImages();
+
+    if (data.totalHits === 0) {
+        Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+    }
+
+    try {
+        if (data.totalHits > 0) {
+            Notify.success(`Hooray! We found ${data.totalHits} images.`);
+
+            appendgalleryMarkup(data.hits);
+            loadMoreBtn.enable();
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
 };
 
 // ====================================onLoadMore========================================
 
-function onLoadMore() {
+async function onLoadMore() {
+
     imagesApiFetch.incrementPage();
-    imagesApiFetch.fetchPixabayImages()
-        .then(data => {
-            appendgalleryMarkup(data.hits);
-            scroll()
+    const data = await imagesApiFetch.fetchPixabayImages();
+
+    try {
+        appendgalleryMarkup(data.hits);
+        scroll()
         
-            if (data.hits.length < 40) {
-                Notify.warning("We're sorry, but you've reached the end of search results.");
-                refs.messageEl.classList.remove('is-hidden');
-                loadMoreBtn.hide()
-            }
-        })
-};
+        if (data.hits.length < 40) {
+            Notify.warning("We're sorry, but you've reached the end of search results.");
+            refs.messageEl.classList.remove('is-hidden');
+            loadMoreBtn.hide()
+        };
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
 
 // ====================================appendgalleryMarkup=&=cleargalleryMarkup========================================
 
